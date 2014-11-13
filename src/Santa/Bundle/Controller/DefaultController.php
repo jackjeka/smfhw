1b750753416cfdb82a7b5b29c35fa132701cf4f0
jack_jeka <angie.or.jackie@gmail.com>
2014-11-14 01:32:42 +0200

    have generated SantaBundle

---
+++ src/Santa/Bundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Santa\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SantaBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
