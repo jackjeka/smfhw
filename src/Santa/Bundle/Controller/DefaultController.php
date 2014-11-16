c471f510ceb2b43209c6b57f3ec84730e3171ec4
jack_jeka <angie.or.jackie@gmail.com>
2014-11-17 00:06:22 +0200

    created controller, routing and view for horoscope page

--- src/Santa/Bundle/Controller/DefaultController.php
+++ src/Santa/Bundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($sign)
     {
-        return $this->render('SantaBundle:Default:index.html.twig', array('name' => $name));
+
+        return $this->render('SantaBundle:Default:index.html.twig', array('sign' => $sign));
     }
 }
